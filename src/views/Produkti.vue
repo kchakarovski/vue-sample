0654b70321cd427186ed3d7d4618fa6207d47d05
kchakarovski <cakarovskidev@gmail.com>
2020-11-21 15:21:46 +0100

    SS-18 working

--- src/views/Produkti.vue
+++ src/views/Produkti.vue
@@ -1,72 +1,21 @@
 <template>
-  <div id="products">
-    <!-- <slider :inputData="sliderimages" :timerMs="3000" /> -->
-    <div class="produkt-flex">
-      <h1 v-text="selectedData.produkti"></h1>
-      <div
-        id="pijaloci"
-        class="produkt-flex__item"
-        v-for="(value, key) in selectedData.produkt"
-        :key="key"
-      >
-        <img id="pijalocimg" :src="value.img" />
-        <span v-text="value.ime"></span>
-        <span v-text="value.cena"></span>
-        
-        <div id="pici">
-          <div class="pici__item" v-for="(value, key) in value.pici" :key="key">
-            <img :src="value.img" />
-            <span v-text="value.ime"></span>
-            <span v-text="value.cena"></span>
-          </div>
-        </div>
-        <div id="burgeri">
-          <div
-            class="burgeri__item"
-            v-for="(value, key) in value.burgeri"
-            :key="key"
-          >
-            <img :src="value.img" />
-            <span v-text="value.ime"></span>
-            <span v-text="value.cena"></span>
-          </div>
-        </div>
-        <div id="ladnisendvici">
-          <div
-            class="ladnisendvici__item"
-            v-for="(value, key) in value.ladnisendvici"
-            :key="key"
-          >
-            <img :src="value.img" />
-            <span v-text="value.ime"></span>
-            <span v-text="value.cena"></span>
-          </div>
-        </div>
-        <div v-for="(value, key) in value.deserti" :key="key">
-          <span v-text="value.ime"></span>
-          <span v-text="value.cena"></span>
-        </div>
-      </div>
+  <div class="products">
+    <div v-for="(value, key) in selectedData" :key="key">
+      
     </div>
   </div>
 </template>
 <script>
-import { malaga } from "../data/produkti";
-// import Slider from "../components/Slider";
+import { products } from "../data/produkti";
 export default {
   name: "page",
   components:{
-    // Slider,
   },
   data() {
     return {
       slug: "",
       selectedData: [],
-      main: malaga, // -----
-      sliderimages: [
-        "https://images.hdqwalls.com/download/pizza-baked-chesse-spicy-7t-1920x1080.jpg",
-        "https://i.ibb.co/2SvXnwQ/164-1645988-wallpaper-burger-food-fast-food-burger-special.jpg",
-      ],
+      main: products, // -----
     };
   },
   watch: {
@@ -76,8 +25,6 @@ export default {
           this.selectedData = element;
         }
       });
-      // react to route changes...
-      //   this.title = to.params.slug;
     },
   },
 };
